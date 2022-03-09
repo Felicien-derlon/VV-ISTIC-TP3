8d0f1af6e2fd7a083b43af35416b22449215d35a
« <>
2022-03-09 17:39:44 +0100

    Code exercice 3 date

--- code/tp3-date/src/main/java/fr/istic/vv/Date.java
+++ code/tp3-date/src/main/java/fr/istic/vv/Date.java
@@ -1,17 +1,90 @@
 package fr.istic.vv;
 
 class Date implements Comparable<Date> {
+    private static final int[] nbJours={31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private int day, month, year;
 
-    public Date(int day, int month, int year) { }
+    public Date(int day, int month, int year) throws Exception{
+        if (!isValidDate(day, month, year)) throw new Exception("mois illégal");
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
 
-    public static boolean isValidDate(int day, int month, int year) { return false; }
+    public static boolean isValidDate(int day, int month, int year) {
+        if( month<0 || month>12) return false;
+        if( (month == 2 && isLeapYear(year) && day>0 && day<=29) || ( day>0 && day<=nbJours[month-1])) return true;;
+        return false;
+    }
 
-    public static boolean isLeapYear(int year) { return false; }
+    public static boolean isLeapYear(int year) {
+        return (year%4==0 && year%100!=0) || year%400==0;
+    }
 
-    public Date nextDate() { return null; }
+    public Date nextDate() throws Exception {
+        int day = this.day, month = this.month, year = this.year;
+        try{
+            ++day;
+            isValidDate(day,month,year);
+        }catch(Exception ex){
+            try{
+                ++month;
+                day = 1;
+                isValidDate(day,month,year);
+            }catch(Exception ex1){
+                try{
+                    ++year;
+                    month = 1;
+                    day = 1;
+                    isValidDate(day,month,year);
+                }catch(Exception ex2){
+                    System.out.println("Something went wrong.");
+                }
+            }
+        }
+        return new Date(day, month, year);
+    }
 
-    public Date previousDate() { return null; }
+    public Date previousDate() throws Exception {
+        int day = this.day, month = this.month, year = this.year;
+        try{
+            --day;
+            isValidDate(day,month,year);
+        }catch(Exception ex){
+            try{
+                --month;
+                day = nbJours[month - 1];
+                if(isLeapYear(year) &&  month == 2) ++day;
+                isValidDate(day,month,year);
+            }catch(Exception ex1){
+                try{
+                    --year;
+                    month = 12;
+                    day = 31;
+                    isValidDate(day,month,year);
+                }catch(Exception ex2){
+                    System.out.println("Something went wrong.");
+                }
+            }
+        }
+        return new Date(day, month, year);
+    }
 
-    public int compareTo(Date other) { return 0; }
+    public int compareTo(Date other) {
+        if (this.day < other.getDay() || this.month < other.getMonth() || this.year < other.getYear()) return -1;
+        else if (this.day > other.getDay() || this.month > other.getMonth() || this.year > other.getYear()) return 1;
+        return 0;
+    }
 
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getYear() {
+        return year;
+    }
 }
--- code/tp3-date/src/test/java/fr/istic/vv/DateTest.java
+++ code/tp3-date/src/test/java/fr/istic/vv/DateTest.java
@@ -6,5 +6,53 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DateTest {
 
+    @Test
+    public void isValidate(){
+           assertTrue(Date.isValidDate(20,10,2022) );
+    }
 
+    @Test
+    public void isNotValidate(){
+        assertFalse(Date.isValidDate(35,10,2022) );
+    }
+
+    @Test
+    public void nextDateValidate(){
+        try {
+            Date date = new Date(03,10,2022);
+            assertEquals(date.nextDate().compareTo(new Date(04,10,2022)), 0);
+        } catch (Exception e) {
+            fail("problème dans le changement de la date");
+        }
+    }
+
+    @Test
+    public void previousDateValidate(){
+        try {
+            Date date = new Date(03,10,2022);
+            assertEquals(date.previousDate().compareTo(new Date(02,10,2022)), 0);
+        } catch (Exception e) {
+            fail("problème dans le changement de la date");
+        }
+    }
+
+    @Test
+    public void compareToSup(){
+        try {
+            Date date = new Date(03,10,2022);
+            assertEquals(date.compareTo(new Date(01,10,2022)), 1);
+        } catch (Exception e) {
+            fail("problème dans le compareTo de la date");
+        }
+    }
+
+    @Test
+    public void compareToInf(){
+        try {
+            Date date = new Date(03,10,2022);
+            assertEquals(date.compareTo(new Date(05,10,2022)), -1);
+        } catch (Exception e) {
+            fail("problème dans le compareTo de la date");
+        }
+    }
 }
